09d56a0bbdcca7f4eb0535a593b046c82972c9c5
Paweł Piórkowski <pawel@piorkowski.eu>
2025-11-04 14:04:34 +0100

    init

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -46,7 +46,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('no-reply@piorkowski.eu', 'Rejestracja - Nyska Liga Bowlingowa'))
+                    ->from(new Address('no-reply@nyskaligabowlingowa.pl', 'Rejestracja - Nyska Liga Bowlingowa'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
